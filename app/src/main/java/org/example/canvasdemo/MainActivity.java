613db877e69d4f74ed73f0270b8c0757e477157e
Csvejle <ceciliavejle@outlook.dk>
2017-03-23 13:04:27 +0100

    Dokumentation opdateringer

--- app/src/main/java/org/example/canvasdemo/MainActivity.java
+++ app/src/main/java/org/example/canvasdemo/MainActivity.java
@@ -1,40 +1,293 @@
 package org.example.canvasdemo;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends Activity {
 	
-	MyView myView;
+	MyView myView; //Mit custom view
+
+	//Timers til, movement og game længde
+	private Timer moveTimer;
+	private Timer gameTimer;
+
+	//Køre spillet?
+	private boolean running = false;
+
+	//Tids counter
+	private int counter = 90;
+
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		Button button = (Button) findViewById(R.id.moveButton);
+
+
+		//Finder det View, hvor spillet skal tegnes
 		myView = (MyView) findViewById(R.id.gameView);
+		myView.setActivity(this); //Giver viewet en reference til aktiviteten
+
+		if (savedInstanceState!=null)
+		{
+			//Genskaber værdier der er gemt
+			ArrayList<Enemy> enemies = savedInstanceState.getParcelableArrayList("enemies");
+			ArrayList<GoldCoin> coins = savedInstanceState.getParcelableArrayList("coins");
+			myView.recreateGame(savedInstanceState.getInt("xPos"), savedInstanceState.getInt("yPos"),
+					coins,
+					savedInstanceState.getInt("level"), savedInstanceState.getInt("points"),
+					savedInstanceState.getInt("highScore"), enemies, savedInstanceState.getBoolean("gameover"),savedInstanceState.getInt("direction"));
+
+			running = savedInstanceState.getBoolean("running");
+			counter = savedInstanceState.getInt("count");
+		}
+		else
+		{
+			running = true; // Starter spillet, hvis ingen stadier er gemt.
+		}
+
+
+		//Finder knapper
+		Button buttonRight = (Button) findViewById(R.id.moveRight);
+		Button buttonUp = (Button) findViewById(R.id.moveUp);
+		Button buttonDown = (Button) findViewById(R.id.moveDown);
+		Button buttonLeft = (Button) findViewById(R.id.moveLeft);
+
 		//listener of our pacman
-		button.setOnClickListener(new OnClickListener() {
-			
+		buttonRight.setOnClickListener(new OnClickListener() {
+
 			@Override
 			public void onClick(View v) {
-				myView.moveRight(10);
+				myView.setDirection(Direction.RIGHT);
+
+				//myView.moveRight(5);
+				//updatePointBoard();
 			}
 		});
-		
-		
+		buttonLeft.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				myView.setDirection(Direction.LEFT);
+				//updatePointBoard();
+			}
+		});
+		buttonUp.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				myView.setDirection(Direction.UP);
+				//myView.moveUp(5);
+				//updatePointBoard();
+			}
+		});
+		buttonDown.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				myView.setDirection(Direction.DOWN);
+				//myView.moveDown(5);
+				//updatePointBoard();
+			}
+		});
+
+		//updatePointBoard();
+
+
+		//make a new timer
+		moveTimer = new Timer();
+
+		//We will call the timer 5 times each second
+		moveTimer.schedule(new TimerTask() {
+			@Override
+			public void run() {
+				TimerMethod();
+			}
+
+		}, 0, 150); //0 indicates we start now, 200
+		//is the number of miliseconds between each call
+
+
+		//Game timer laves
+		gameTimer = new Timer();
+
+		//Game timer planlægges
+		//Starter med det samme, da 0, som først int parameter angiver.
+		//Kør en gang i sec. (1000 milisekunder), som andet int parameter angiver.
+		gameTimer.schedule(new TimerTask() {
+			@Override
+			public void run() {
+				updateGameTime();
+			}
+		}, 0, 1000);
 	}
 
 	@Override
+	protected void onStop() {
+
+		//Sørger for, at hvis brugeren stopper appen, så håndters dette, sådan der holdes pause for spillet.
+		super.onStop();
+		running = !running;
+		invalidateOptionsMenu(); //Gentegner menuen, da nederste punkt er ændret
+	}
+
+
+	@Override
+	protected void onResume() {
+		//Sørger for, at hvis brugeren genåbner appen, så håndters dette, sådan spillet fortsættes.
+
+		running = !running;
+		invalidateOptionsMenu(); //Gentegner menuen, da nederste punkt er ændret
+		super.onResume();
+	}
+
+	//*
+	// Opdater spilletiden, og sætter game over, hvis tiden er brugt.
+	// Ved hvert level fornyes tiden i det custom view
+	// */
+	private void updateGameTime(){
+		if (running)
+		{
+			counter--;
+
+			//Tiden er gået
+			if(counter == 0){
+				myView.setGameOver(true);
+				invalidateOptionsMenu(); //Gentegner menuen, da nederste punkt er ændret
+			}
+		}
+	}
+
+	private void TimerMethod()
+	{
+		//This method is called directly by the timer
+		//and runs in the same thread as the timer.
+
+		//We call the method that will work with the UI
+		//through the runOnUiThread method.
+		this.runOnUiThread(Timer_Tick);
+	}
+
+
+	private Runnable Timer_Tick = new Runnable() {
+		public void run() {
+
+			//This method runs in the same thread as the UI.
+			// so we can draw
+			if (running)
+			{
+				//Beregner hastighed på pacman, hvilket er baseret på level
+				int speed = myView.getLevel()*7;
+				if(speed > 60) {
+					speed = 60;
+				}
+				myView.move(speed); //Bevæger pacman og fjender.
+			}
+
+		}
+	};
+
+
+	/** Opdater game board, hvilket er der, hvor brugeren ser spillets status. */
+	public void updatePointBoard(){
+
+		int currentPoints = myView.getPoints();
+		TextView pointsBoard = (TextView) findViewById(R.id.points);
+		pointsBoard.setText("Points: " + currentPoints + ", Level: " + myView.getLevel() +", Time left: " + counter + " sec." +
+				", Highscore: " +myView.getHighScore());
+
+
+	}
+
+	/** Resætter game timer **/
+	public void resetGameTimer(int time){
+		gameTimer.cancel();
+		gameTimer = new Timer();
+		gameTimer.schedule(new TimerTask() {
+			@Override
+			public void run() {
+				updateGameTime();
+			}
+		}, 0, 1000);
+
+		counter = time;
+	}
+
+	/** Resætter move timer **/
+	public void resetMoveTimer(){
+		moveTimer.cancel();
+		moveTimer = new Timer();
+		moveTimer.schedule(new TimerTask() {
+			@Override
+			public void run() {
+				TimerMethod();
+			}
+
+		}, 0, 200);
+
+		running = true;
+	}
+
+	public void timerStop(){
+		//Logik der skal til, hvis game over
+
+		gameTimer.cancel();
+		gameTimer.cancel();
+		running = false;
+
+		if(counter < 0){
+			counter = 0;
+		}
+	}
+
+	/**
+	 * Beregner teksten der skal bruges til nederste menupunkt.
+	 * Hvilket er en knap til, at pause og fortsæt spillet.
+	 * **/
+	private String calcRunBtnText()
+	{
+		String titleItem = "Pause";
+		if(!running) {
+			titleItem = "Resume";
+		}
+
+		return titleItem;
+	}
+	
+	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
+
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
+
+		MenuItem item= menu.findItem(R.id.runStatus);
+		item.setTitle(calcRunBtnText());
+
 		return true;
+	}
+
+	@Override
+	public boolean onPrepareOptionsMenu(Menu menu) {
+
+		MenuItem item= menu.findItem(R.id.runStatus);
+
+		if(myView.isGameOver())	{
+			item.setEnabled(false);
+		}
+		else {  item.setEnabled(true); }
+
+		return super.onPrepareOptionsMenu(menu);
 	}
 
 	@Override
@@ -42,10 +295,47 @@ public class MainActivity extends Activity {
 		// Handle action bar item clicks here. The action bar will
 		// automatically handle clicks on the Home/Up button, so long
 		// as you specify a parent activity in AndroidManifest.xml.
+
+
 		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
+		if(id == R.id.newgame) {
+			myView.newGame(); //Starter nyt spil
+			updatePointBoard(); //Opdater game board, hvilket visningen af spillets status.
 		}
+		else if(id == R.id.shareScore) {
+			//Deling af highscore og point
+			Intent sendIntent = new Intent();
+			sendIntent.setAction(Intent.ACTION_SEND);
+			sendIntent.putExtra(Intent.EXTRA_TEXT, "Pacman highscore is " + myView.getHighScore() + ", og my points is " +myView.getPoints()+ ".");
+			sendIntent.setType("text/plain");
+			startActivity(sendIntent);
+		}
+		else if(id == R.id.runStatus){
+			//Pauser eller fortsætter spillet
+			running = !running;
+			item.setTitle(calcRunBtnText());
+		}
+
 		return super.onOptionsItemSelected(item);
+	}
+
+
+	//** Gemmer værder der skal bruges til, at genskabe spil, når skærm orientation ændres. /når app dør. *//
+	@Override
+	protected void onSaveInstanceState(Bundle outState) {
+		super.onSaveInstanceState(outState);
+
+		outState.putParcelableArrayList("coins", myView.getCoins());
+		outState.putInt("xPos", myView.getPacx());
+		outState.putInt("yPos", myView.getPacy());
+		outState.putInt("level", myView.getLevel());
+		outState.putInt("points", myView.getPoints());
+		outState.putInt("highScore", myView.getHighScore());
+		outState.putBoolean("running", running);
+		outState.putInt("count", counter);
+
+		outState.putBoolean("gameover", myView.isGameOver());
+		outState.putParcelableArrayList("enemies", myView.getEnemies());
+		outState.putInt("direction", myView.getDirection().ordinal());
 	}
 }
--- app/src/main/java/org/example/canvasdemo/MyView.java
+++ app/src/main/java/org/example/canvasdemo/MyView.java
@@ -1,75 +1,586 @@
 package org.example.canvasdemo;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Typeface;
 import android.util.AttributeSet;
 import android.view.View;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class MyView extends View{
+	MainActivity activity; //Reference til, den aktivitet der bruger viewet, da det skal bruges metoder derfra.
 	
-	Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.pacman);
+	Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.pacman); //Pacman bitmap
+    Bitmap gostBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.gost); //Gost bitmap
+
+
     //The coordinates for our dear pacman: (0,0) is the top-left corner
-	int pacx = 50;
-    int pacy = 400;
-    int h,w; //used for storing our height and width
-    
-    public void moveRight(int x)
-    {
-    	//still within our boundaries?
-    	if (pacx+x+bitmap.getWidth()<w)
-    		pacx=pacx+x;
-    	invalidate(); //redraw everything - this ensures onDraw() is called.
-    }
+	private int pacx = 50;
+	private int pacy = 400;
+	private int h,w; //used for storing our height and width
+
+	private int highScore;
+
+    //Bruges til highscore beregning/hentning og gemning
+	private SharedPreferences sharedPref;
+	private String savedhightScore;
+
+    private boolean gameOver = false; //Indiker om game over
+    private int points; //Point counter
+	private int level = 1; //Level counter
+
+	private Direction direction = Direction.LEFT; //retningen pacmanen skal bevæge sig
+
+	//Mønter pacman kan tage
+	ArrayList<GoldCoin> coins = new ArrayList<>();
+
+    //Enemies ArrayList -> så muligt med flere fjender
+    ArrayList<Enemy> enemies = new ArrayList<>();
 
 	/* The next 3 constructors are needed for the Android view system,
 	when we have a custom view.
 	 */
 	public MyView(Context context) {
 		super(context);
-		
+
 	}
-	
 	public MyView(Context context, AttributeSet attrs) {
 		super(context,attrs);
 	}
-	
-	
 	public MyView(Context context, AttributeSet attrs, int defStyleAttr)
 	{
 		super(context,attrs,defStyleAttr);
 	}
 
+
 	//In the onDraw we put all our code that should be
 	//drawn whenever we update the screen.
 	@Override
-	protected void onDraw(Canvas canvas) {  
-		//Here we get the height and weight
+	protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas); //Kald til super klassens metode
+
+		//Får højde og bredde på canvas
 		h = canvas.getHeight();
 		w = canvas.getWidth();
-		System.out.println("h = "+h+", w = "+w);
+
+        //Highscore sættelse
+		this.sharedPref = activity.getPreferences(Context.MODE_PRIVATE);
+		this.savedhightScore = activity.getString(R.string.saved_high_score);
+		this.highScore = sharedPref.getInt(savedhightScore, 0);
+
+
 		//Making a new paint object
 		Paint paint = new Paint();
+
+
 		//setting the color
-		paint.setColor(Color.RED);
+		paint.setColor(Color.rgb(218,165,32)); //Den farve mønterne får
 		canvas.drawColor(Color.WHITE); //clear entire canvas to white color
-		//drawing a line from (0,0) -> (300,200)
-		canvas.drawLine(0,0,300,200,paint);
-		paint.setColor(Color.GREEN);
-		canvas.drawLine(0,200,300,0,paint);
-		
-		//setting the color using the format: Transparency, Red, Green, Blue
-		paint.setColor(0xff000099);
-		
-		//drawing a circle with radius 20, and center in (100,100) 
-		canvas.drawCircle(100,100,20,paint);
-		
-		
-		canvas.drawBitmap(bitmap, pacx, pacy, paint);
-		super.onDraw(canvas);
+
+
+		//Et nyt spil laves hvis ingen mønter er der, da det her betyder spillet ikke er startet endnu
+		if(coins.size() == 0){
+			newGame();
+		}
+
+        if(points == coins.size())
+        {
+            addCoins(); //Tilføjer 10 nye mønter, da alle er taget.
+            level += 1; //Går til næste level
+            activity.resetGameTimer(90-(level*10)); //Resætter spille timer
+        }
+
+
+		for(GoldCoin coin:coins)
+		{
+			//Tjekker om mønten er taget = mønt skal tegnes, hvis den ikke er
+			if(!coin.isTaken()) {
+
+				//Tjekker mønten er på skærmen, og laver ny position, hvis ikke er.
+                // vilket er muligt, ved ændring at skærm orientation
+				if(coin.getX() <= 20 || coin.getX() > w-20){
+					coin.setX(randomInt(w-20));
+				}
+
+				if(coin.getY() <= 20 || coin.getY() > h-20){
+					coin.setY(randomInt(h-20));
+				}
+
+				//Tegner mønt
+				canvas.drawCircle(coin.getX(), coin.getY(), 10,paint);
+			}
+		}
+
+		//Tilpasser spillets fjender ift. niveau
+        switch (level)
+        {
+            case 3:
+                enemies.get(0).setLevel(Level.NEUTRAL);
+
+                if(enemies.size() == 1) {
+                    addEnemy();
+                }
+                break;
+            case 6:
+                enemies.get(0).setLevel(Level.HARD);
+                enemies.get(1).setLevel(Level.NEUTRAL);
+
+                if(enemies.size() == 2) {
+                    addEnemy();
+                }
+                break;
+            case 9:
+                enemies.get(0).setLevel(Level.SUPER_HARD);
+                enemies.get(1).setLevel(Level.HARD);
+                if(enemies.size() == 2) {
+                   addEnemy();
+                }
+
+                break;
+            case 10:
+                enemies.get(1).setLevel(Level.SUPER_HARD);
+                enemies.get(2).setLevel(Level.HARD);
+                if(enemies.size() == 3) {
+                    addEnemy();
+                }
+
+                break;
+        }
+
+
+        for(Enemy enemy:enemies){
+
+            //Tjekker fjender er på skærmen, og laver ny position, hvis ikke er.
+            //Hvilket er muligt, ved ændring at skærm orientation
+            if(enemy.getXPos() <= 0 || enemy.getXPos() > w-gostBitmap.getWidth()){
+                enemy.setXPos(randomInt(w-gostBitmap.getWidth()));
+            }
+
+            if(enemy.getYPos() <= 0 || enemy.getYPos() > h-gostBitmap.getHeight()){
+                enemy.setYPos(randomInt(h-gostBitmap.getHeight()));
+            }
+
+            //Tegner fjende
+            canvas.drawBitmap(gostBitmap, enemy.getXPos(), enemy.getYPos(), paint); //Tegner fjenen på skærmen
+        }
+
+
+		//Tjekker pacman er på skærmen, og laver ny position, hvis ikke er.
+        //Hvilket er muligt, ved ændring at skærm orientation
+		if(pacx < 0 || pacx > w-bitmap.getWidth()){ pacx =  randomInt(w-bitmap.getWidth()); }
+		if(pacy < 0 || pacy > h-bitmap.getHeight()){ pacy = randomInt(h-bitmap.getHeight()); }
+
+		activity.updatePointBoard();
+		canvas.drawBitmap(bitmap, pacx, pacy, paint); //Tegner pacmanen på skærmen
+
+        if(isGameOver()) {
+            activity.timerStop(); //Stopper timers
+
+            //Paint til tekst
+            Paint paint2 = new Paint();
+
+            //Sætter bg farve på paint
+            paint2.setColor(Color.WHITE);
+
+            //Sætter canvas paint
+            canvas.drawPaint(paint2);
+
+            //Ændre skrift
+            paint2.setColor(Color.BLACK);
+            paint2.setTextSize(175);
+            paint2.setTypeface(Typeface.create(Typeface.DEFAULT, Typeface.BOLD));
+
+            //Beregner position
+            int txtX = 50;
+            int txtY = 70;
+
+
+            if(h>w){
+                canvas.rotate(45); //Roter, hvis portait mode
+            }
+            else
+            {
+                txtY= 200;
+            }
+
+            //tegner game over tekst
+            canvas.drawText("GAME OVER", txtX, txtY, paint2);
+        }
 	}
 
+	/** Metode til, at gentegne, da det skal bruges mange gange. */
+	public void reDraw(){
+        invalidate();
+    }
+
+	/**Laver og returner random int
+       Metoden er lavet, da der skal bruges random mange gange. **/
+	private int randomInt(int maks){
+		Random random = new Random();
+		return random.nextInt(maks);
+	}
+
+
+    /**
+     * Flytter pacman og fjender
+     * Beregner point
+     * Beregner om, man er tæt på fjende
+     * Pacman og fjernder gå til modsat hjørne, hvis vil være uden for skærm efter flytning **/
+    public void move(int x){
+
+        //Looper alle fjender
+        for(Enemy enemy:enemies)
+        {
+            //Får postion
+            int enemyYPost =  enemy.getYPos();
+            int enemyXPost =  enemy.getXPos();
+
+            //Angiver default hastighed
+            int speed = 8;
+
+            //Hastigheds tilpassning ift. level
+            speed += enemy.getLevel().ordinal();
+
+
+            //Beregner om fjende skal skifte retning
+            if(enemy.getDirectionChangeCounter() <= 0){
+                enemy.moveCalc();
+            }
+            else
+            {
+                //Ændre på, hvornår der skal skiftets retning
+                enemy.decreaseLength();
+            }
+
+            //Finder x og y postion på fjende
+            switch (enemy.getDirection()){
+                case DOWN:
+                    if (enemyYPost+speed+gostBitmap.getHeight()<h)
+                        enemyYPost=enemyYPost+speed;
+                    else
+                        enemyYPost = 0;
+                    break;
+                case UP:
+                    if (enemyYPost-speed > 0)
+                        enemyYPost=enemyYPost-speed;
+                    else
+                        enemyYPost = h-gostBitmap.getHeight();
+                    break;
+                case LEFT:
+                    if (enemyXPost-speed > 0)
+                        enemyXPost=enemyXPost-speed;
+                    else
+                        enemyXPost = w-gostBitmap.getWidth();
+                    break;
+                case RIGHT:
+                    if (enemyXPost+speed+gostBitmap.getWidth()<w)
+                        enemyXPost=enemyXPost+speed;
+                    else
+                        enemyXPost = 0;
+                    break;
+            }
+
+            //Sætter ny postion
+            enemy.setYPos(enemyYPost);
+            enemy.setXPos(enemyXPost);
+        }
+
+        //Beregner ny pacman placering
+		switch (direction){
+            case DOWN:
+                if (pacy+x+bitmap.getHeight()<h)
+                    pacy=pacy+x;
+                else
+                    pacy = 0;
+                break;
+            case UP:
+                if (pacy-x > 0)
+                    pacy=pacy-x;
+                else
+                    pacy = h - bitmap.getHeight();
+                break;
+            case LEFT:
+                if (pacx-x > 0)
+                    pacx=pacx-x;
+                else
+                    pacx = w-bitmap.getWidth();
+                break;
+            case RIGHT:
+                if (pacx+x+bitmap.getWidth()<w)
+                    pacx=pacx+x;
+                else
+                    pacx = 0;
+                break;
+		}
+
+		//Beregner point
+        calcPoint();
+
+
+        //Indiker GAME OVER, hvis ramt fjende
+        if(isNearEnemy()){
+            GameOver();
+        }
+
+
+        //Bemærk rækkefælge på ovenstående
+        //Det betyder, at hvis man rammer en fjende, og er tæt på en mønt,
+        //så får man point for mønten.
+        // - for at være sød. :P
+
+        //Gentegner spillet
+        reDraw();
+	}
+
+	public Direction getDirection() {
+		return direction;
+	}
+    public void setDirection(Direction direction){
+        this.direction = direction;
+    }
+
+	public int getLevel() {
+		return level;
+	}
+
+    public void setActivity(MainActivity activity) {
+        this.activity = activity;
+    }
+
+	public int getPoints(){
+		return points;
+	}
+	/**Opdater points ift., hvor pacman er.
+	   Der gives rt point pr. taget mønt **/
+	private void calcPoint() {
+
+        //Variabel til ny point beregning
+		int tempPoints = 0;
+
+
+        //Looper GoldCoins
+		for(GoldCoin coin:coins)
+		{
+            //Beregner afstanden til mønt
+			double distance = Math.hypot(coin.getX()-(pacx+(bitmap.getWidth()/2)), coin.getY()-(pacy+(bitmap.getHeight()/2)));
+
+            //Beregner om, der du er tæt på en mønt, og sætter mønten til taken, hvis man er.
+			if(distance <= 60)
+			{
+				coin.setTaken(true);
+			}
+
+			//Beregninger om, der skal tilføjes et point
+			if(coin.isTaken()){
+				tempPoints += 1;
+			}
+		}
+
+		//Sætter point til det, som beregningen ovenover har medført
+		points = tempPoints;
+
+
+        //Opdater highscore, hvis du har fået flere points
+		if(highScore < points) {
+			SharedPreferences.Editor editor = sharedPref.edit();
+			editor.putInt(savedhightScore, points);
+			editor.commit();
+			highScore = points;
+		}
+	}
+
+
+	public ArrayList<GoldCoin> getCoins() {
+		return new ArrayList<>(coins);
+	}
+    /** Tilføjer 10 mønter */
+	private void addCoins() {
+		for (int i = 0; i < 10; i++) {
+			coins.add(new GoldCoin(randomInt(w-20),
+					randomInt(h-20), false));
+		}
+	}
+
+    public ArrayList<Enemy> getEnemies() {
+        return new ArrayList<>(enemies);
+    }
+    /** Tilføjer en fjende */
+    private void addEnemy(){
+        enemies.add(new Enemy(Level.ESAY, randomInt(w-gostBitmap.getWidth()),
+                randomInt(h-gostBitmap.getHeight())));
+    }
+    /** Beregninger om, man er tæt på en fjende. **/
+    private boolean isNearEnemy() {
+        boolean res = false;
+
+        //Looper alle fjender
+        for(Enemy enemy:enemies)
+        {
+            //Beregninger afstand til fjenden
+            double distance = Math.hypot((enemy.getXPos()+(gostBitmap.getWidth()/2))-(pacx+(bitmap.getWidth()/2)),
+                    (enemy.getYPos()+(bitmap.getHeight()/2)-(pacy+(bitmap.getHeight()/2))));
+
+            //Er man tæt på fjenden?
+            if(distance <= 80)
+            {
+                res = true; //Angiver ja
+                break; //Stopper loop
+            }
+        }
+
+        return res;
+    }
+
+
+	public int getPacx() {
+		return pacx;
+	}
+	public int getPacy() {
+		return pacy;
+	}
+
+	public int getHighScore() {
+		return highScore;
+	}
+
+    public boolean isGameOver()
+    {
+        return gameOver;
+    }
+    public void setGameOver(boolean nyValue)
+    {
+        gameOver = nyValue;
+
+    }
+    /**Gør sådan, at spillet gør til game over stadiet,
+    hvilket vil sige, at der vises en kæmpe game over tekst */
+    public void GameOver() {
+        gameOver = true;
+        activity.invalidateOptionsMenu();
+    }
+
+    //New game
+    public void newGame(){
+        coins.clear();
+        enemies.clear();
+
+        pacx = randomInt(w-bitmap.getWidth());
+        pacy = randomInt(h-bitmap.getHeight());
+
+        addCoins();
+        addEnemy();
+        points = 0;
+        level = 1;
+        gameOver = false;
+        activity.resetGameTimer(90-(level*10));
+        activity.resetMoveTimer();
+        reDraw();
+    }
+
+	//Genskaber spil
+	public void recreateGame(int x, int y,
+							 ArrayList<GoldCoin> coinsSaved, int level, int points,
+                             int highScore, ArrayList<Enemy> enemies, boolean gameover, int direction){
+
+
+
+        this.enemies = enemies;
+
+        for (Enemy enemey:enemies){
+            //Bytter rundt på x og y, sådan fjenden vises "samme sted".
+            int tempX = enemey.getXPos();
+            enemey.setXPos(enemey.getYPos());
+            enemey.setYPos(tempX);
+        }
+
+        coins = coinsSaved;
+
+		for (GoldCoin coin:coins){
+			//Bytter rundt på x og y, sådan mønter vises "samme sted".
+			int tempX = coin.getX();
+			coin.setX(coin.getY());
+			coin.setY(tempX);
+		}
+
+
+		//Bytter rundt på x og y, sådan pacman vises "samme sted".
+		pacx = y;
+		pacy = x;
+
+
+        //Settter yderligere værdier, sådan spillet kan fortsætter, hvor man slap
+        this.direction = Direction.values()[direction];
+		this.highScore = highScore;
+		this.level = level;
+		this.points = points;
+        this.gameOver = gameover;
+
+        //Gør der gentegnes
+        reDraw();
+	}
+
+
+
+
+
+
+    //Move metoder
+    @Deprecated
+    public void moveRight(int x)
+    {
+        //still within our boundaries?
+        if (pacx+x+bitmap.getWidth()<w)
+            pacx=pacx+x;
+        else
+            pacx = 0;
+
+        calcPoint();
+        reDraw(); //redraw everything - this ensures onDraw() is called.
+    }
+
+    @Deprecated
+    public void moveLeft(int x)
+    {
+        //still within our boundaries?
+        if (pacx-x > 0)
+            pacx=pacx-x;
+        else
+            pacx = w-bitmap.getWidth();
+
+        calcPoint();
+        reDraw(); //redraw everything - this ensures onDraw() is called.
+    }
+
+    @Deprecated
+    public void moveDown(int x)
+    {
+        //still within our boundaries?
+        if (pacy+x+bitmap.getHeight()<h)
+            pacy=pacy+x;
+        else
+            pacy = 0;
+
+        calcPoint();
+        reDraw(); //redraw everything - this ensures onDraw() is called.
+    }
+
+    @Deprecated
+    public void moveUp(int x)
+    {
+        //still within our boundaries?
+        if (pacy-x > 0)
+            pacy=pacy-x;
+        else
+            pacy = h - bitmap.getHeight();
+
+        calcPoint();
+        reDraw(); //redraw everything - this ensures onDraw() is called.
+    }
 }
